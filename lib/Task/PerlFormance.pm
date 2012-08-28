e0d681917fbf4c836c45a721077f07b7121f6b90
Steffen Schwigon <schwigon@cpan.org>
2012-08-28 14:47:32 +0000

    Import of SCHWIGON/Task-PerlFormance-0.001 from CPAN.
    gitpan-cpan-distribution: Task-PerlFormance
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         SCHWIGON/Task-PerlFormance-0.001.tar.gz
    gitpan-cpan-author:       SCHWIGON
    gitpan-cpan-maturity:     released

---
+++ lib/Task/PerlFormance.pm
@@ -0,0 +1,131 @@
+use strict;
+use warnings;
+package Task::PerlFormance;
+# git description: 902acbc
+
+BEGIN {
+  $Task::PerlFormance::AUTHORITY = 'cpan:SCHWIGON';
+}
+{
+  $Task::PerlFormance::VERSION = '0.001';
+}
+# ABSTRACT: Max dependencies for Benchmark::Perl::Formance
+
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::PerlFormance - Max dependencies for Benchmark::Perl::Formance
+
+=head1 VERSION
+
+version 0.001
+
+=head1 TASK CONTENTS
+
+=head2 perlformance
+
+=head3 L<forks>
+
+=head3 L<Benchmark::Perl::Formance>
+
+=head3 L<Tapper::TestSuite::Benchmark::Perl::Formance>
+
+=head2 SpamAssassin
+
+=head3 L<DB_File>
+
+=head3 L<Net::IP>
+
+=head3 L<Net::DNS>
+
+=head3 L<NetAddr::IP>
+
+=head3 L<HTML::Parser>
+
+=head3 L<Mail::SpamAssassin>
+
+=head2 OO
+
+=head3 L<MooseX::Declare>
+
+=head3 L<Moose>
+
+=head3 L<Mouse>
+
+=head2 RxCmp
+
+=head3 L<POSIX::Regex>
+
+=head3 L<ExtUtils::CppGuess>
+
+=head3 L<re::engine::Lua>
+
+=head3 L<re::engine::LPEG>
+
+=head3 L<re::engine::Plan9>
+
+=head3 L<re::engine::Oniguruma>
+
+=head3 L<re::engine::RE2>
+
+=head2 Regex
+
+=head3 L<Regexp::Common>
+
+=head3 L<DateTime::Calendar::Mayan>
+
+=head3 L<Locale::US>
+
+=head3 L<HTTP::Headers>
+
+=head3 L<URI>
+
+=head2 Shootout
+
+=head3 L<Math::GMP>
+
+=head2 Primes
+
+=head3 L<Crypt::Primes>
+
+=head3 L<Math::Primality>
+
+=head2 DPath
+
+=head3 L<Clone>
+
+=head3 L<Devel::Size>
+
+=head3 L<Data::DPath>
+
+=head2 P6STD
+
+=head3 L<Text::Balanced>
+
+=head3 L<YAML::XS>
+
+=head3 L<Encode>
+
+=head2 PerlCritic
+
+=head3 L<Perl::Critic>
+
+1;
+
+=head1 AUTHOR
+
+Steffen Schwigon <ss5@renormalist.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steffen Schwigon.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
